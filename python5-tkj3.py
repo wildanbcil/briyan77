ff824aae91d764a1b3f045e9a6a731e577f080b8
wildan <111479965+wildanbcil@users.noreply.github.com>
2022-09-01 20:23:26 -0700

    briyan tkj 3
    tugas pemdas

---
+++ python5-tkj3.py
@@ -0,0 +1,41 @@
+#rumus persegi sisi * sisi
+
+
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print("luas persegi adalah" , luas_p)
+
+
+#Rumus segitiga A * T / 2
+alas_s = 20
+tinggi_s = 10
+luas_s = alas_s * tinggi_s
+
+print("luas segitiga adalah" , luas_s)
+
+
+#Rumus jajar genjang alas * tinggi
+alas_j = 10
+tinggi_j = 20
+luas_j = alas_j * tinggi_j
+
+
+print("luas jajar gemjamg adalah" , luas_j)
+
+
+
+#rumus lingkaran phi * r * r
+phi = 22/7
+r = 14
+luas_1 = phi * r * r
+
+print ("luas lingkaran adalah" , luas_1)
+
+print("\ni.luas persegi")
+sisi = int(input("masukan nilai sisi : "))
+luas_p = sisi * sisi
+print("luas persegi adalah" , luas_p)
+
+
