df19645be4842108eb951353bc35e77324246c96
wildan <111479965+wildanbcil@users.noreply.github.com>
2022-09-04 22:55:05 -0700

    tugas pemdas
    briyan goklas yudika s

---
+++ python6-tkj3.py
@@ -0,0 +1,21 @@
+#Dolar ke rupiah
+dolar = int(input("masukan nilai dolar: " ))
+rupiah = dolar * 14900
+
+print ("dolar ke rupiah" , rupiah )
+
+
+#Yen ke rupiah
+yen = int(input("masukan nilai yen: " ))
+rupiah = yen * 106
+
+print ("yen ke rupiah" , rupiah)
+
+
+#won ke rupiah
+won = int(input("masukan nilai won: " ))
+rupiah = won * 1357
+
+
+print ("won ke rupiah" , rupiah)
+                  
